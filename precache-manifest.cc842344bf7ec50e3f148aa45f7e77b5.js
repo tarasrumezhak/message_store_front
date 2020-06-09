077af779e4f6f6aeceb60a4d7d27537aa54486d3
Taras <rumezhak.taras@gmail.com>
2020-06-09 16:50:45 +0300

    Updates

---
+++ precache-manifest.cc842344bf7ec50e3f148aa45f7e77b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8793b734f11474292b57cd4a0511be33",
+    "url": "/message_store_front/index.html"
+  },
+  {
+    "revision": "d2935f91026dc130f88d",
+    "url": "/message_store_front/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e64c7cc67382c37f260a",
+    "url": "/message_store_front/static/js/2.21791918.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/message_store_front/static/js/2.21791918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2935f91026dc130f88d",
+    "url": "/message_store_front/static/js/main.3a971472.chunk.js"
+  },
+  {
+    "revision": "a0eeefac977bcb09d196",
+    "url": "/message_store_front/static/js/runtime-main.1efa4081.js"
+  }
+]);
